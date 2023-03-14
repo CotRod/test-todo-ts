ce3a01c141132f228afc3d7af39865a54f7a44d3
Rodnoy Konstantin <kotrod@gmail.com>
2023-03-14 15:53:15 +0300

    prepare project before starting development

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classes from './app.module.scss';
+
+const {app, container, title} = classes;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={app}>
+      <div className={container}>
+      <h1 className={title}>Simple todo list</h1>
+      </div>
     </div>
   );
 }
