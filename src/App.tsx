42c1750f752f6c35a6f36faf7693da25c4e7ec55
Rodnoy Konstantin <kotrod@gmail.com>
2023-03-14 17:50:35 +0300

    add list of items

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import classes from './app.module.scss';
-import { Button, Divider, Form, Input } from 'antd';
+import { Badge, Button, Card, Divider, Form, Input, List } from 'antd';
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 
-const {app, container, title} = classes;
+const { app, container, title } = classes;
+
 interface FormValues {
   title: string;
   text: string;
@@ -15,10 +17,16 @@ interface todoItem {
 }
 
 function App() {
-  const [items, setItems] = useState<todoItem[]>([]);
+  const jsonItems = localStorage.getItem('todos');
+  let itemsFromLocalStorage = [];
+  if (jsonItems) {
+    itemsFromLocalStorage = JSON.parse(jsonItems);
+  }
+
+  const [items, setItems] = useState<todoItem[]>(itemsFromLocalStorage);
   const [form] = Form.useForm();
 
-  useEffect(()=> {
+  useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(items));
   }, [items]);
 
@@ -40,7 +48,7 @@ function App() {
         <Form
           layout={'vertical'}
           onFinish={onFinish}
-          style={{width: '100%'}}
+          style={{ width: '100%' }}
           form={form}
         >
           <Form.Item
@@ -48,7 +56,10 @@ function App() {
             name={'title'}
             required
           >
-            <Input/>
+            <Input
+              maxLength={30}
+              showCount
+            />
           </Form.Item>
           <Form.Item
             label={'What should I do'}
@@ -67,11 +78,57 @@ function App() {
             </Button>
           </Form.Item>
         </Form>
-        <Divider />
-
+        <Divider/>
+        <List
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 2,
+            md: 3,
+            lg: 4,
+            xl: 4,
+            xxl: 5,
+          }}
+          style={{width: '100%'}}
+          dataSource={items}
+          renderItem={(item) => (
+            <List.Item>
+              <Badge.Ribbon
+                text={getReadableDateFromIso(item.date)}
+                style={{top: 0}}
+              >
+              <Card
+                title={item.title}
+                actions={[
+                  <EditOutlined key="edit" />,
+                  <DeleteOutlined key="delete" />,
+                ]}
+              >
+                {item.text}
+              </Card>
+              </Badge.Ribbon>
+            </List.Item>
+          )
+          }
+        />
       </div>
     </div>
   );
+}
+
+function getReadableDateFromIso(isoDate: string) {
+  const date = new Date(isoDate);
+  const hours = date.getHours();
+  const minutes = padWithZero(date.getMinutes());
+  const seconds = padWithZero(date.getSeconds());
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  return `${hours}:${minutes}:${seconds} - ${day}/${month}/${year}`
+}
+
+function padWithZero(num: number) {
+  return `${num}`.padStart(2, '0');
 }
 
 export default App;
